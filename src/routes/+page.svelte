e843f3bf658cbb5a5f7216202ef3021c8e011a48
moolenbeek <emoolenbeek@gmail.com>
2024-08-13 20:44:10 -0400

    add todo list and style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,29 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script>
+	import { focusTrap } from '@skeletonlabs/skeleton';
+	export let data;
+</script>
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
-			<li><code class="code">/src/app.postcss</code> - app wide css</li>
+<div class="container m-auto max-w-xs">
+	<h1>todos</h1>
+
+	<form use:focusTrap={true} method="POST" action="?/create">
+		<label>
+			add a todo:
+			<input data-focusindex="0" name="description" autocomplete="off" class="input" type="text" placeholder="Input" />
+		</label>
+	</form>
+
+	<ul class="list my-4">
+		{#each data.todos as todo (todo.id)}
 			<li>
-				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
+				<form method="POST" action="?/delete">
+					<div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
+						<input type="hidden" name="id" value={todo.id} />
+						<span class="mx-4">{todo.description}</span>
+						<button class="variant-soft-error" aria-label="Mark as complete">-</button>
+					</div>
+				</form>
 			</li>
-		</ul>
-	</div>
+		{/each}
+	</ul>
 </div>
